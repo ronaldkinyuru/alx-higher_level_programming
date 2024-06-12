deb7ff9e85d42811eb488c706792f77e179d2403
ronaldkinyuru <ronaldkinyuru@gmail.com>
2024-06-12 20:08:56 +0300

    all

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-const fs = require('fs');
+const fs = require('fs'); 
+//import biultin fs
 const sourcePath1 = process.argv[2];
 
 fs.readFile(sourcePath1, 'utf8', function (err, data) {
